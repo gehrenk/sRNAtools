7cf22a8cd10896c092518653a5324d269802ad27
gehrenk <gehrenk@stanford.edu>
2020-12-27 19:02:00 -0800

    Fixed typo

--- R/alph.R
+++ R/alph.R
@@ -10,7 +10,7 @@
 #' @examples
 #' alph(test.dss)
 alph <- function(x){
-  par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=inTRUE)
+  par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
   barplot(ShortRead::alphabetByCycle(x)[,1:barwidth(x)], col=cols, space=0, cex.names=1.4,
           names.arg=1:barwidth(x), cex.axis=1.5, ylab="Frequency", xlab="position", cex.lab=1.5)
   legend(barwidth(x), length(x), bases, fill=cols, cex=1.3)}
